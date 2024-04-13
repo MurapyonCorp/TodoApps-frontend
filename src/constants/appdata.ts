b30d204b84027f635e779874c65e251c85395d5a
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2024-04-13 17:23:48 +0900

    初期設定完了

---
+++ src/constants/appdata.ts
@@ -0,0 +1,12 @@
+/**
+ * アプリ基本情報
+ * @type {Object}
+ */
+
+export const APP_DATA = {
+  APP_NAME: "TodoApp",
+  APP_URL: 'http://localhost:3000',
+  APP_ICON: '@/app/favicon.ico',
+  APP_DESCRIPTION: '説明',
+  LOCALE: 'ja',
+};
