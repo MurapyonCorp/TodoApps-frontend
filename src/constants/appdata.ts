870bee669a486b3f03bae009e3dfc72b2152f70b
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2024-04-14 13:56:15 +0900

    header footer構想中

--- src/constants/appdata.ts
+++ src/constants/appdata.ts
@@ -6,7 +6,7 @@
 export const APP_DATA = {
   APP_NAME: "TodoApp",
   APP_URL: 'http://localhost:3000',
-  APP_ICON: '@/app/favicon.ico',
+  APP_ICON: '/favicon.ico',
   APP_DESCRIPTION: '説明',
   LOCALE: 'ja',
 };
