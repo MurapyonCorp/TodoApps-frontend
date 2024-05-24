a68e0f87bcc8d3cb27029726b2a3d366f69c6300
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2024-05-24 16:59:00 +0900

    edit

--- src/app/themeSwitch.tsx
+++ src/app/themeSwitch.tsx
@@ -18,33 +18,22 @@ export const ThemeSwitch = () => {
   }
 
   return (
-    <>
-      {/* <select
-        value={theme}
-        onChange={(e) => setTheme(e.target.value)}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      >
-        <option value="system">System</option>
-        <option value="dark">Dark</option>
-        <option value="light">Light</option>
-      </select> */}
-      <Dropdown
-        label={theme === "dark" ? <IoSunny size={25} /> : <IoMoon size={25} />}
-        arrowIcon={false}
-        dismissOnClick={true}
-        color={""}
-        size={"xs"}
-      >
-        <Dropdown.Item icon={IoSunny} onClick={() => setTheme("light")}>
-          Light
-        </Dropdown.Item>
-        <Dropdown.Item icon={IoMoon} onClick={() => setTheme("dark")}>
-          Dark
-        </Dropdown.Item>
-        <Dropdown.Item icon={RiComputerFill} onClick={() => setTheme("system")}>
-          System
-        </Dropdown.Item>
-      </Dropdown>
-    </>
+    <Dropdown
+      label={theme === "dark" ? <IoSunny size={25} /> : <IoMoon size={25} />}
+      arrowIcon={false}
+      dismissOnClick={true}
+      color={""}
+      size={"xs"}
+    >
+      <Dropdown.Item icon={IoSunny} onClick={() => setTheme("light")}>
+        Light
+      </Dropdown.Item>
+      <Dropdown.Item icon={IoMoon} onClick={() => setTheme("dark")}>
+        Dark
+      </Dropdown.Item>
+      <Dropdown.Item icon={RiComputerFill} onClick={() => setTheme("system")}>
+        System
+      </Dropdown.Item>
+    </Dropdown>
   );
 };
