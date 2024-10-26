912a28fd748557bcaf779e98598bffbba77af94a
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2024-10-26 15:57:54 +0900

    favicon削除

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import { UI_DATA } from "@/constants/uidata";
 import { ThemeSwitch } from "@/app/themeSwitch";
 
@@ -7,13 +6,6 @@ export const Header = () => {
   return (
     <div className="static flex justify-center">
       <h1 className="font-bold text-3xl text-center">{UI_DATA.HOME_TITLE}</h1>
-      <Image
-        src={"/images/shisa.png"}
-        alt={"shisa"}
-        width={41}
-        height={41}
-        className="ml-4"
-      />
       <div className="absolute right-20">
         <ThemeSwitch />
       </div>
