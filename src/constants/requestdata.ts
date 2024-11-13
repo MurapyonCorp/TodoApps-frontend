f4c240ab472a1a8d78a7650965929a3a2aaff99b
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2024-11-13 13:52:10 +0900

    Webページ完成

--- src/constants/requestdata.ts
+++ src/constants/requestdata.ts
@@ -2,16 +2,41 @@
  * HTTPリクエストのURL
  * @type {Object}
  */
+
 export const REQUEST_DATA = {
-  TODO_GET: 'http://localhost:3000/todos',
-  TODO_POST: 'http://localhost:3000/todos',
-  TODO_PUT: 'http://localhost:3000/todos',
-  TODO_DELETE: 'http://localhost:3000/todos/',
-}
+  TODO_GET:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/todos"
+      : "https://todoapps-backend.onrender.com/todos",
+  TODO_POST:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/todos"
+      : "https://todoapps-backend.onrender.com/todos",
+  TODO_PUT:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/todos"
+      : "https://todoapps-backend.onrender.com/todos",
+  TODO_DELETE:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/todos/"
+      : "https://todoapps-backend.onrender.com/todos/",
+};
 
 export const REQUEST_TIME_DATA = {
-  COUNTUP_GET: 'http://localhost:3000/countup-timers',
-  COUNTUP_POST: 'http://localhost:3000/countup-timers',
-  COUNTUP_PUT: 'http://localhost:3000/countup-timers',
-  COUNTUP_DELETE: 'http://localhost:3000/countup-timers/',
-}
+  COUNTUP_GET:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/countup-timers"
+      : "https://todoapps-backend.onrender.com/countup-timers",
+  COUNTUP_POST:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/countup-timers"
+      : "https://todoapps-backend.onrender.com/countup-timers",
+  COUNTUP_PUT:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/countup-timers"
+      : "https://todoapps-backend.onrender.com/countup-timers",
+  COUNTUP_DELETE:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/countup-timers/"
+      : "https://todoapps-backend.onrender.com/countup-timers/",
+};
